de05dcaa02616879c9f4c68ccc94a481bdbb5606
Andrew Chelladurai <13437037+andrewchelladurai@users.noreply.github.com>
2023-02-09 22:49:58 +0530

    Add uptime

--- config-dir/.config/polybar/scripts/sysinfo.sh
+++ config-dir/.config/polybar/scripts/sysinfo.sh
@@ -14,3 +14,6 @@ xset -q | grep Caps | tr -s " " | cut -d" " -f 3-5
 xset -q | grep Caps | tr -s " " | cut -d" " -f 7-9
 # Scroll Lock
 xset -q | grep Caps | tr -s " " | cut -d" " -f 11-13
+# Uptime
+uptime --pretty
+

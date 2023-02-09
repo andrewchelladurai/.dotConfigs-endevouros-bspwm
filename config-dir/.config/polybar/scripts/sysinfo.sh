9eef42ae6a9530ad8ef86c320be422565b0e1b1a
Andrew Chelladurai <13437037+andrewchelladurai@users.noreply.github.com>
2023-02-09 21:31:30 +0530

    Add command to display Modifier Keys

--- config-dir/.config/polybar/scripts/sysinfo.sh
+++ config-dir/.config/polybar/scripts/sysinfo.sh
@@ -7,3 +7,10 @@ echo "Now It's "$(date +%X" "%Z" of "%a", "%d"-"%b"-"%Y)
 # Battery
 # Sound
 # Modifier Key Status
+
+# Caps Lock
+xset -q | grep Caps | tr -s " " | cut -d" " -f 3-5
+# Num Lock
+xset -q | grep Caps | tr -s " " | cut -d" " -f 7-9
+# Scroll Lock
+xset -q | grep Caps | tr -s " " | cut -d" " -f 11-13
